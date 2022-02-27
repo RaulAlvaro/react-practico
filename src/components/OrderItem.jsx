68ceca1fc5b5b3ace358ecdada7627f41f1152a3
Raul Vidal <raulvidal@MacBook-Pro-de-Raul.local>
2022-02-27 10:32:39 -0500

    [ADD] Github Actions

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import iconClose from '@icons/icon_close.png'
+import '../styles/OrderItem.scss';
+
+const OrderItem = ({ product }) => {
+	const {removeFromCart} = useContext(AppContext)
+
+	const handleRmove = product => {
+		removeFromCart(product)
+	}
+
+	return (
+		<div className="OrderItem">
+			<figure>
+				<img src={product.images[0]} alt={product.title} />
+			</figure>
+			<p>{product.title}</p>
+			<p>{product.price}</p>
+			<img src={iconClose} alt="close" onClick={() => handleRmove(product)}/>
+		</div>
+	);
+}
+
+export default OrderItem;
